d6235e9855339efcecb66238177062504f6a6505
Markus Richter <richter.markus.s@web.de>
2022-07-07 14:24:46 +0200

    Padding mistake corected

--- src/tasks/task3.py
+++ src/tasks/task3.py
@@ -6,34 +6,38 @@ def padding(message, block):
     if (len(message) % block) == 0:
         padding = 16
     else:
-        padding = (len(message) % block)
+        padding = block - (len(message) % block)
     message = message.encode()
     message = binascii.hexlify(message).decode()
     for i in range(padding):
-        message += hex(padding)[2:]
+        message += hex(padding).replace("x", "")
     return message
 
 
 def create_task3():
     # create task2 layout OTP
     layout = [
-        [sg.Text('Padding')],
-        [sg.Input('Message', key='-MESSAGE-')],
-        [sg.Input('Block length', key='-LENGTH-')],
-        [sg.Button('Submit')],
-        [sg.InputText('Result!', key='-RESULT-',use_readonly_for_disable=True, size=600)],
-        [sg.Button('finish')]
+        [sg.Text("Padding")],
+        [sg.Input("Message", key="-MESSAGE-")],
+        [sg.Input("Block length", key="-LENGTH-")],
+        [sg.Button("Submit")],
+        [
+            sg.InputText(
+                "Result!", key="-RESULT-", use_readonly_for_disable=True, size=600
+            )
+        ],
+        [sg.Button("finish")],
     ]
     # create window for the task
-    window_task3 = sg.Window('Padding', layout, size=(1200, 600))
+    window_task3 = sg.Window("Padding", layout, size=(1200, 600))
 
     while True:
         event, values = window_task3.read()
-        if event in (None, 'finish'):
+        if event in (None, "finish"):
             break
-        elif event == 'Submit':
-            message = str(values['-MESSAGE-'])
-            block = int(values['-LENGTH-'])
+        elif event == "Submit":
+            message = str(values["-MESSAGE-"])
+            block = int(values["-LENGTH-"])
             result = padding(message, block)
-            window_task3['-RESULT-'].update(result)
+            window_task3["-RESULT-"].update(result)
     window_task3.close()
